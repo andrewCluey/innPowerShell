94b74ea4551a0f8bf1296e5a8eab557aafc4cef0
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-07-08 15:27:41 +0100

    completed script

--- getRoleAssignments.ps1
+++ getRoleAssignments.ps1
@@ -1,5 +1,38 @@
-$subscriptions = Get-AzSubscription
+function Get-SubscriptionAssignments {
+    param (
+        [string] $exportFile
+    )
+    <#
+        .SYNOPSIS
+        Gets all IAm role assignments for signed in to subscriptions.
 
-$assignments = Get-AzRoleAssignment
-   
+        .DESCRIPTION
 
+        .PARAMETER exportFile
+        The path where the returned CSV file shoudl be saved.
+
+        .EXAMPLE
+        Get-SubscriptionAssignments -ExportFile "./assignments.csv"
+
+    #>
+
+    $subscriptions = Get-AzSubscription
+    $roleAssignmentReport = @()
+    
+    foreach ($sub in $subscriptions) {
+        $Assignments = Get-AzRoleAssignment
+        foreach ($Assignment in $Assignments) {
+            $signInName =  $assignment.signinName
+            $roleDefintionName = $Assignment.RoleDefinitionName
+            $scope = $Assignment.Scope
+            $roleAssignmentObject = New-Object PSObject
+            $roleAssignmentObject | Add-Member -type NoteProperty -name SignInName -Value $signInName
+            $roleAssignmentObject | Add-Member -type NoteProperty -name RoleName -Value $roleDefintionName
+            $roleAssignmentObject | Add-Member -type NoteProperty -name Scope -Value $scope
+            $roleAssignmentObject | Add-Member -type NoteProperty -name Subscription -Value $sub.name
+            $roleAssignmentReport += $roleAssignmentObject
+        }
+    }
+
+$roleAssignmentReport | Export-Csv $exportFile -NoTypeInformation
+}
